dfb4d8bddf8219281a2c68fe059abc32b485af33
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-08-06 19:59:48 +0300

    #2 bouncing ball - with struct

--- main.c
+++ main.c
@@ -3,10 +3,23 @@
 #define SCREEN_WIDTH 1020
 #define SCREEN_HEIGHT 700
 
+typedef struct {
+  Vector2 pos;
+  Vector2 vel;
+  float rad;
+  float mass;
+  Color color;
+} Ball;
+
 int main(void) {
-  Vector2 ball_pos = {SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f};
-  Vector2 ball_speed = {5.0f, 4.0f};
-  int ball_rad = 20;
+
+  Ball ball;
+  ball.pos.x = SCREEN_WIDTH / 2.0f;
+  ball.pos.y = SCREEN_HEIGHT / 2.0f;
+  ball.rad = 20;
+  ball.vel.x = 4.0f;
+  ball.vel.y = 5.0f;
+  ball.color = RED;
   
   Rectangle random_rec = {510, 350, 30, 30};
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pac Xon");
@@ -15,17 +28,17 @@ int main(void) {
 
   while(!WindowShouldClose()){
 
-      ball_pos.x += ball_speed.x;
-      ball_pos.y += ball_speed.y;
+      ball.pos.x += ball.vel.x;
+      ball.pos.y += ball.vel.y;
 
-      if(ball_pos.x >= (SCREEN_WIDTH - ball_rad) || ball_pos.x <= (0 + ball_rad)) ball_speed.x = -ball_speed.x;
-      if(ball_pos.y >= (SCREEN_HEIGHT - ball_rad) || ball_pos.y <= (0 + ball_rad)) ball_speed.y = -ball_speed.y;
+      if(ball.pos.x >= (SCREEN_WIDTH - ball.rad) || ball.pos.x <= (0 + ball.rad)) ball.vel.x = -ball.vel.x;
+      if(ball.pos.y >= (SCREEN_HEIGHT - ball.rad) || ball.pos.y <= (0 + ball.rad)) ball.vel.y = -ball.vel.y;
 
     
     BeginDrawing();
     ClearBackground(BLACK);
     DrawRectangleRec(random_rec, WHITE);
-    DrawCircle(ball_pos.x, ball_pos.y, ball_rad, RED);
+    DrawCircle(ball.pos.x, ball.pos.y, ball.rad, ball.color);
     EndDrawing();
   }
 
