23619820af0f7d0bfe1590f5ba26c0d8a765c19a
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-08-07 15:00:04 +0300

    cleaned the code

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 
 #include "raylib.h"
+#include "raymath.h"
 #define SCREEN_WIDTH 1020
 #define SCREEN_HEIGHT 700
 
@@ -11,15 +12,18 @@ typedef struct {
   Color color;
 } Ball;
 
+Ball ball_init(Vector2 pos, Vector2 vel, float rad, float mass, Color color);
+Ball ball_update(Ball ball);
+
 int main(void) {
 
-  Ball ball;
-  ball.pos.x = SCREEN_WIDTH / 2.0f;
-  ball.pos.y = SCREEN_HEIGHT / 2.0f;
-  ball.rad = 20;
-  ball.vel.x = 4.0f;
-  ball.vel.y = 5.0f;
-  ball.color = RED;
+  Ball ball = ball_init(
+			(Vector2){(SCREEN_WIDTH / 2.0f), (SCREEN_HEIGHT / 2.0f)},
+			(Vector2){4.0f, 5.0f},
+			20,
+			20,
+			RED
+			);
   
   Rectangle random_rec = {510, 350, 30, 30};
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pac Xon");
@@ -28,13 +32,8 @@ int main(void) {
 
   while(!WindowShouldClose()){
 
-      ball.pos.x += ball.vel.x;
-      ball.pos.y += ball.vel.y;
 
-      if(ball.pos.x >= (SCREEN_WIDTH - ball.rad) || ball.pos.x <= (0 + ball.rad)) ball.vel.x = -ball.vel.x;
-      if(ball.pos.y >= (SCREEN_HEIGHT - ball.rad) || ball.pos.y <= (0 + ball.rad)) ball.vel.y = -ball.vel.y;
-
-    
+    ball = ball_update(ball);
     BeginDrawing();
     ClearBackground(BLACK);
     DrawRectangleRec(random_rec, WHITE);
@@ -45,4 +44,26 @@ int main(void) {
   CloseWindow();
 
   return 0;
+}
+
+
+
+Ball ball_init(Vector2 pos, Vector2 vel, float rad, float mass, Color color) {
+  return (Ball){pos, vel, rad, mass, color};
+}
+
+Ball ball_update(Ball ball) {
+  ball.pos.x += ball.vel.x;
+  ball.pos.y += ball.vel.y;
+
+
+  if(ball.pos.x >= (SCREEN_WIDTH - ball.rad) || ball.pos.x <= (0 + ball.rad))
+    {
+      ball.vel.x = -ball.vel.x;
+    }
+  if(ball.pos.y >= (SCREEN_HEIGHT - ball.rad) || ball.pos.y <= (0 + ball.rad))
+    {
+      ball.vel.y = -ball.vel.y;
+    }
+  return ball ;
 }
