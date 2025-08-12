65d819efd309b4c59cfb17dfc9fe0558bc240657
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-08-12 21:06:36 +0300

    added player

--- main.c
+++ main.c
@@ -13,6 +13,12 @@ typedef struct {
   float rad;
   float mass;
   Color color;
+
+  //extra for the player
+  int cell_x, cell_y;
+  int target_x, target_y;
+  bool moving;
+  float speed;
 } Ball;
 
 //WALLS
@@ -25,6 +31,7 @@ typedef enum {
 // grid
 WallState grid[GRID_ROWS][GRID_COLS];
 
+
 //functions
 Ball ball_init(Vector2 pos, Vector2 vel, float rad, float mass, Color color);
 Ball ball_update(Ball ball, float dt, WallState grid[GRID_ROWS][GRID_COLS]);
@@ -32,16 +39,15 @@ Vector2 get_attraction_force(Ball a, Ball b, float G);
 void apply_force(Ball *ball, Vector2 force, float dt);
 void draw_grid(Ball ball);
 void init_grid();
+void player_init(Ball *player, int start_cell_x, int start_cell_y, float speed,
+                 float rad, Color color);
+void player_move(Ball *player, int dx, int dy);
+void player_update(Ball *player, float dt);
+static inline bool is_solid_cell(int row, int col);
 
 int main(void) {
   float G = 1000.0f;
-  Ball ball = ball_init(
-			(Vector2){44.0f, 55.0f},
-			(Vector2){4.0f, 5.0f},
-			20,
-			20,
-			GREEN
-			);
+  Ball ball = ball_init((Vector2){200.0f, 300.0f}, (Vector2){4.0f, 5.0f}, 20, 20, GREEN);
 
   //Ball attractor = ball_init((Vector2){(SCREEN_WIDTH / 2.0f), (SCREEN_HEIGHT / 2.0f)},(Vector2){0, 0}, 40, 100000, RED); 
   
@@ -49,20 +55,31 @@ int main(void) {
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pac Xon");
   init_grid();
 
+  Ball player;
+  player_init(&player, 2, 2, 220.0f, CELL_SIZE * 0.4f, DARKGREEN);
+
   SetTargetFPS(60);
 
   while(!WindowShouldClose()){
     float dt = GetFrameTime();
     //Vector2 force = get_attraction_force(ball, attractor, G);
     //apply_force(&ball, force, dt);
+    if (!player.moving)
+      {
+	if (IsKeyPressed(KEY_RIGHT)) player_move(&player, +1,  0);
+	if (IsKeyPressed(KEY_LEFT))  player_move(&player, -1,  0);
+	if (IsKeyPressed(KEY_UP))    player_move(&player,  0, -1);
+	if (IsKeyPressed(KEY_DOWN))  player_move(&player,  0, +1);
+      }
 
-
+    player_update(&player, dt);
     ball = ball_update(ball, dt, grid);
     //attractor = ball_update(attractor, dt);
     BeginDrawing();
     ClearBackground(BLACK);
     draw_grid(ball);
-    DrawCircle(ball.pos.x, ball.pos.y, ball.rad, ball.color);
+    DrawCircleV(ball.pos, ball.rad, ball.color);
+    DrawCircleV(player.pos, player.rad, player.color);
     //DrawCircle(attractor.pos.x, attractor.pos.y, attractor.rad, attractor.color);
     EndDrawing();
   }
@@ -75,34 +92,40 @@ int main(void) {
 
 // # 1
 Ball ball_init(Vector2 pos, Vector2 vel, float rad, float mass, Color color) {
-  return (Ball){pos, vel, rad, mass, color};
+  return (Ball){pos, vel, rad, mass, color, false};
 }
 
 // # 2
 Ball ball_update(Ball ball, float dt, WallState grid[GRID_ROWS][GRID_COLS]) {
+
+  Vector2 next_pos = {
+    ball.pos.x + ball.vel.x,
+    ball.pos.y + ball.vel.y
+  };
+
+  
+  int next_cell_x = next_pos.x / CELL_SIZE;
+  int next_cell_y = next_pos.y / CELL_SIZE;
+
   
   int cell_x = ball.pos.x / CELL_SIZE;
   int cell_y = ball.pos.y / CELL_SIZE;
   WallState current = grid[cell_y][cell_x];
 
 
-  ball.pos.x += ball.vel.x ;//* dt;
-  ball.pos.y += ball.vel.y ;//* dt;
-
-  if(current)
-    {
-      
-    }
-
-
-  if(ball.pos.x >= (SCREEN_WIDTH - ball.rad) || ball.pos.x <= (0 + ball.rad))
+  // if(ball.pos.x >= (SCREEN_WIDTH - ball.rad) || ball.pos.x <= (0 + ball.rad))
+  if(grid[cell_y][next_cell_x] == WALL_SOLID)
     {
       ball.vel.x = -ball.vel.x;
     }
-  if(ball.pos.y >= (SCREEN_HEIGHT - ball.rad) || ball.pos.y <= (0 + ball.rad))
+  //if(ball.pos.y >= (SCREEN_HEIGHT - ball.rad) || ball.pos.y <= (0 + ball.rad))
+  if(grid[next_cell_y][cell_x] == WALL_SOLID)
     {
       ball.vel.y = -ball.vel.y;
     }
+  
+  ball.pos.x += ball.vel.x ;//* dt;
+  ball.pos.y += ball.vel.y ;//* dt;
   return ball ;
 }
 
@@ -125,13 +148,7 @@ void apply_force(Ball *ball, Vector2 force, float dt)
 // # 5
 void draw_grid(Ball ball)
 {
-  Vector2 next_pos = {
-    ball.pos.x + ball.vel.x,
-    ball.pos.y + ball.vel.y
-  };
-
-  int next_cell_x = next_pos.x / CELL_SIZE;
-  int next_cell_y = next_pos.y / CELL_SIZE;
+  
   
   for(int row = 0; row < GRID_ROWS; row++)
     {
@@ -147,10 +164,6 @@ void draw_grid(Ball ball)
 	    case WALL_SOLID: c = GRAY; break;
 	    }
 
-	  if(row == next_cell_y && col == next_cell_x){
-	    c = PINK;
-	  }
-
 	  DrawRectangle(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE, c);
 	}
     }
@@ -159,7 +172,7 @@ void draw_grid(Ball ball)
 
 // # 6
 void init_grid() {
-  
+
   for(int row = 0; row < GRID_ROWS; row++)
     {
       for(int col = 0; col < GRID_COLS; col++)
@@ -170,5 +183,74 @@ void init_grid() {
 	    grid[row][col] = WALL_BLANK;
 	}
     }
-  
+
+  grid[1][16] = WALL_SOLID;
+  grid[2][16] = WALL_SOLID;
+  grid[3][16] = WALL_SOLID;
+  grid[4][16] = WALL_SOLID;
+  grid[5][16] = WALL_SOLID;
+  grid[6][16] = WALL_SOLID;
+  grid[7][16] = WALL_SOLID;
+  grid[8][16] = WALL_SOLID;
+  grid[9][16] = WALL_SOLID;  
+}
+
+// # 7
+
+void player_init(Ball *player, int start_cell_x, int start_cell_y, float speed,
+                 float rad, Color color)
+{
+  player->cell_x = start_cell_x;
+  player->cell_y = start_cell_y;
+  player->target_x = start_cell_x;
+  player->target_y = start_cell_y;
+  player->pos = (Vector2){start_cell_x * CELL_SIZE + CELL_SIZE * 0.5f,
+			  start_cell_y * CELL_SIZE + CELL_SIZE * 0.5f};
+  player->vel = (Vector2){0, 0};
+  player->rad = rad;
+  player->mass = 100.0f;
+  player->color = color;
+  player->moving = false;
+  player->speed = speed;
+}
+
+// # 8
+void player_move(Ball *player, int dx, int dy) {
+  if (player->moving) return;
+  int nx = player->cell_x + dx;
+  int ny = player->cell_y + dy;
+
+  if (nx < 0 || nx >= GRID_COLS || ny < 0 || ny >= GRID_ROWS) return;
+
+  player->target_x = nx;
+  player->target_y = ny;
+  player->moving = true;
+}
+
+
+// # 9
+void player_update(Ball *player, float dt)
+{
+  if (!player->moving) return;
+
+  Vector2 target_pos = (Vector2) {
+    player->target_x * CELL_SIZE + CELL_SIZE * 0.5f,
+    player->target_y * CELL_SIZE + CELL_SIZE * 0.5f
+  };
+
+  Vector2 dir = Vector2Subtract(target_pos, player->pos);
+  float dist = Vector2Length(dir);
+
+  if (dist <= player->speed * dt) {
+    player->pos = target_pos;
+    player->cell_x = player->target_x;
+    player->cell_y = player->target_y;
+    player->moving = false;
+
+    //if (grid[player->cell_y][player->cell_x] == WALL_BLANK) make_tile();
+  } else {
+
+    Vector2 step = Vector2Scale(Vector2Normalize(dir), player->speed * dt);
+    player->pos = Vector2Add(player->pos, step);
+  }
 }
